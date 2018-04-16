2131895c48cfb78db150f38c32107f64c25e17d5
Rick Gallegos <duulketariakan@yahoo.com>
2018-04-15 19:20:34 -0700

    Modularize folder structure and lint files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import 'semantic-ui-css/semantic.min.css';
+
+import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
